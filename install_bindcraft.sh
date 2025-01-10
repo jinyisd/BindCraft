ea5cdefa6fb3d7e44a501133f0fdb27f476a166c
jinyisd <jinyi2023@shanghaitech.edu.cn>
2025-01-10 17:47:53 +0800

    Update install_bindcraft.sh

--- install_bindcraft.sh
+++ install_bindcraft.sh
@@ -103,7 +103,7 @@ wget -O "${params_file}" "https://storage.googleapis.com/alphafold/alphafold_par
 
 # extract AF2 weights
 tar tf "${params_file}" >/dev/null 2>&1 || { echo -e "Error: Corrupt AlphaFold2 weights download"; exit 1; }
-tar -xvf "${params_file}" -C "${params_dir}" || { echo -e "Error: Failed to extract AlphaFold2weights"; exit 1; }
+tar --no-same-owner -xvf "${params_file}" -C "${params_dir}" || { echo -e "Error: Failed to extract AlphaFold2weights"; exit 1; }
 [ -f "${params_dir}/params_model_5_ptm.npz" ] || { echo -e "Error: Could not locate extracted AlphaFold2 weights"; exit 1; }
 rm "${params_file}" || { echo -e "Warning: Failed to remove AlphaFold2 weights archive"; }
 
